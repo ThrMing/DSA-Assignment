74466d0c6b636e0430e083ec32c19060a5cd4ba0
Alex Cheah <alexkcheah@gmail.com>
2022-06-08 14:07:02 +0800

    Updating Interface

--- BSTtest.java
+++ BSTtest.java
@@ -4,19 +4,9 @@ public class BSTtest {
     public static void main( String args[] ) {
         BinaryTree tree = new BinaryTree();
 
-        System.out.println("Inserting the following values: ");
-
-        tree.insertNode(12);
-        tree.insertNode(4);
-        tree.insertNode(8);
-        tree.insertNode(20);
-        tree.insertNode(23);
-        tree.deleteNode(4);
-        tree.deleteNode(12);
-
-        System.out.println(tree.searchNode(12));
-        System.out.println(tree.searchNode(4));
-        System.out.println(tree.searchNode(8));
+        System.out.println("=======================================================");
+        System.out.println("\t\t BINARY SEARCH TREE");
+        System.out.println("=======================================================");
 
 
     }
