7efaa9b1c8396ce31c3899cfc8e451d53008d6cf
dontlookforjojo <107109142+dontlookforjojo@users.noreply.github.com>
2022-06-25 20:00:18 +0800

    Update HashTableTest.java

--- Hashing/HashTableTest.java
+++ Hashing/HashTableTest.java
@@ -3,52 +3,67 @@ import java.util.Scanner;
 /** Class HashTableTest **/
 public class HashTableTest
 {
-    public static void main(String[] args)
+    @SuppressWarnings("unchecked")
+	public static void main(String[] args)
     {    
         Scanner scan = new Scanner(System.in);
+        HashTable table = new HashTable(64);
         System.out.println("Hash Table Test\n\n");
         System.out.println("Enter size");
-        /** Make object of HashTable **/
-        HashTable ht = new HashTable(scan.nextInt() );
- 
+        String key, value;
         char ch;
         /**  Perform HashTable operations  **/
         do    
         {
             System.out.println("\nHash Table Operations\n");
-            System.out.println("1. insert ");
-            System.out.println("2. remove");
-            System.out.println("3. contains");            
-            System.out.println("4. clear");            
+            System.out.println("1. insert(K,V) ");
+            System.out.println("2. get(K) ");
+            System.out.println("3. contain(K) ");  
+            System.out.println("4. remove(K) ");     
+            System.out.println("5. clear");  
+            System.out.print("Enter your command:  ");
  
-            int choice = scan.nextInt();            
-            switch (choice)
+			switch (TextIO.getlnInt())
             {
             case 1 : 
-                System.out.println("Enter integer element to insert");
-                ht.insert( scan.nextInt() ); 
+                System.out.println("\n   Key = ");
+                key = TextIO.getln();
+                System.out.print("   Value = ");
+                value = TextIO.getln();
+                table.insert(key,value);
                 break;                          
             case 2 :                 
-                System.out.println("Enter integer element to delete");
-                ht.delete( scan.nextInt() ); 
+                System.out.println("\n   Key = ");
+                key = TextIO.getln();
+                System.out.println("   Value is " + table.get(key));
                 break;                        
             case 3 : 
-                System.out.println("Enter integer element to check if present");
-                System.out.println("Contains : "+ ht.contains(scan.nextInt() ));
-                break;                                   
-            case 4 : 
-                ht.clear();
+                System.out.println("\n   Key = ");
+                key = TextIO.getln();
+                System.out.println("   containsKey(" + key + ") is " 
+                                             + table.containsKey(key));
+                break;    
+            case 4 :
+            	System.out.print("\n   Key = ");
+                key = TextIO.getln();
+                table.remove(key);
+                break; 
+            case 5 : 
+            	table.clear();
                 System.out.println("Hash Table Cleared\n");
                 break;
             default : 
                 System.out.println("Wrong Entry \n ");
                 break;   
             }
+			
             /** Display hash table **/
-            ht.printTable();  
- 
+			table.printTable();  
+			
             System.out.println("\nDo you want to continue (Type y or n) \n");
             ch = scan.next().charAt(0);                        
         } while (ch == 'Y'|| ch == 'y');  
     }
 }
+
+
