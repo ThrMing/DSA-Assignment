8ed64a56edfd22b7ca985d3d9888500d3c3e4383
TmS1108 <tanmingsiew0811@gmail.com>
2022-06-27 23:36:32 +0800

    Z

--- HashingVer2/Hashing.java
+++ HashingVer2/Hashing.java
@@ -1,5 +1,34 @@
 package HashingVer2;
 
 public class Hashing {
-    
+
+    private int count = 0;
+    private int maxSize;
+    private String[] table;
+
+    public Hashing(int capacity) {
+        // Create a hash table with a specified initial size.
+        table = new String[capacity];
+
+    }
+
+    public void clearTable() {
+        for (int i = 0; i < table.length; i++) {
+            table[i] = "null";
+        }
+    }
+
+    public void printTable() {
+        System.out.println("Hash Table: ");
+        for (int i = 0; i < table.length; i++) {
+            System.out.print(table[i] + " ");
+            count++;
+            if (count == 10) {
+                count = 0;
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+
 }
