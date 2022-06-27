6a69821938e4d3b32a83f69b1f3083df964564ff
Alex Cheah <alexkcheah@gmail.com>
2022-06-27 23:46:20 +0800

    Set up data structures

--- HashingVer2/Hashing.java
+++ HashingVer2/Hashing.java
@@ -1,5 +1,43 @@
 package HashingVer2;
 
+import java.util.LinkedList;
+
 public class Hashing {
-    
+    String[] HashTable;
+    LinkedList<String>[] SCHashTable;
+
+    public Hashing(int n, DataStructures ds, String word) {
+        switch(ds) {
+            case LinearProb:
+                setLinearProb(n, word);
+                break;
+            case DoubleHash:
+                setDoubleHash(n, word);
+                break;
+            case SepChain:
+                setSepChain(n, word);
+                break;
+        }
+    }
+
+    public enum DataStructures {
+        LinearProb, DoubleHash, SepChain
+    }
+
+    private void setLinearProb(int n,String w) {
+        HashTable = new String[n]; 
+
+    }
+
+    private void setDoubleHash(int n,String w) {
+        HashTable = new String[n]; 
+
+    }
+
+    private void setSepChain(int n,String w) {
+        SCHashTable = new LinkedList[n]; 
+
+    }
+
+
 }
